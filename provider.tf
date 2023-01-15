4ec4005557b541cfe19ecf553447e00e4511a69d
yokokawa taiki <himapopo0507@yahoo.co.jp>
2023-01-15 23:09:04 +0900

    first commit

---
+++ provider.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "ap-northeast-1"
+  profile = var.profile
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.50.0"
+    }
+  }
+}
